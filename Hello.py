cd5034ce7253b969b36e3805583dddc7872edf03
Saqib Dar <115392179+SaqibDar112@users.noreply.github.com>
2022-12-11 12:23:40 +0530

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,10 @@
+class details():
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    def sayHello(self):
+        print("Hello",self.name,self.age)
+object1= details("Saqib",18)
+object1.name
+object1.age
+object1.sayHello()
